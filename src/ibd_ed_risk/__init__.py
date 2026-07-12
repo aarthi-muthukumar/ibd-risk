e35e001a5d80eaa03fbb85689bcfbc82da814583
aarthi-muthukumar <78634499+aarthi-muthukumar@users.noreply.github.com>
2026-07-11 19:51:42 -0700

    Update __init__.py
    edited init file for Streamlit deploy

--- src/ibd_ed_risk/__init__.py
+++ src/ibd_ed_risk/__init__.py
@@ -1,10 +1,1 @@
-"""IBD ED risk modeling package."""
-from .config import load_paths
-from .inventory import inventory_hcup
-from .cohort import build_uc_cohort, annotate_uc_cohort, load_ed_core
-from .features import construct_features
-from .outcomes import derive_outcomes
-from .modeling import train_models
-from .evaluation import evaluate_models
-from .survey import survey_design
-from .reporting import write_reports
+"""Utilities for the UC ED admission-risk Streamlit prototype."""
